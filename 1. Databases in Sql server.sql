d646a7eac8a522e5d7934e2880b36a8d19a476e0
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2022-07-01 21:23:29 +0530

    Update 1. Databases in Sql server.sql

--- 1. Databases in Sql server.sql
+++ 1. Databases in Sql server.sql
@@ -8,20 +8,25 @@ It helps the user to find the relevant information frequently.
 
 Use of database
 ----------------
+Manages large amounts of data.
 
-Manages large amounts of data: 
-Accurate: Each database can store the data accurately 
-Easy to update:
-Security:
+Accurate: Each database can store the data accurately.
+
+Easy to update.
+
+Security.
+
 Data integrity: Each database ensures that the stored data is accurate and consistent.
-Easy to research data:
+
+Easy to research data.
+
 
 -------------------------------------------------------------------------------------------------------------------------------------
 
 There Are two types of database:
 -----------------------------------
-system Database
-USer database
+system Database.
+USer database.
 
 -------------------------------------------------------------------------------------------------------------------------------------
 
@@ -30,6 +35,7 @@ System Database:
 The system databases are created automatically while installing the MS SQL Server on our system.
 
 The following are the list of system databases in SQL Server:
+
 Master
 Model
 MSDB
