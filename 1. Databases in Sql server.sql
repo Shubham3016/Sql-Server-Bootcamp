19e9a268070d5ab73f048d413c3bc3e931c97832
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2024-05-04 19:44:43 +0530

    Update 1. Databases in Sql server.sql

--- 1. Databases in Sql server.sql
+++ 1. Databases in Sql server.sql
@@ -4,12 +4,24 @@ create database Demo;
 
 --To know all databases details
 
-select * from sys.databases 
+select * from sys.databases
+  
+exec sp_databases
+
 
 --To know all databases names
 
 select name from sys.databases order by 1 
 
 select name from sys.databases order by name
+
+
+-- To rename database name
+
+alter database demo modify name=Prac
+
+alter database Prac modify name=demo
+
+exec sp_renamedb 'demo','Prac'
 
 
