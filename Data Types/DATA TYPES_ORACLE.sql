94406d808614e25beabb55c2b24b734de72c9379
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2025-08-03 21:26:19 +0530

    Update DATA TYPES_ORACLE.sql

--- Data Types/DATA TYPES_ORACLE.sql
+++ Data Types/DATA TYPES_ORACLE.sql
@@ -55,3 +55,21 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('CLOB: ' || my_clob);
 END;
 /
+
+
+-----------------------------------------------------------------------------------------------------------------------
+
+DECLARE
+    my_date DATE := TRUNC(SYSDATE);  -- current date without time
+    my_timestamp TIMESTAMP := SYSTIMESTAMP;  -- current timestamp with fractional seconds
+    my_tz TIMESTAMP WITH TIME ZONE := SYSTIMESTAMP;  -- current timestamp with time zone
+    my_tsl TIMESTAMP WITH LOCAL TIME ZONE := SYSTIMESTAMP;  -- current timestamp with local time zone
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('DATE: ' || TO_CHAR(my_date, 'YYYY-MM-DD'));
+    DBMS_OUTPUT.PUT_LINE('TIMESTAMP: ' || TO_CHAR(my_timestamp, 'YYYY-MM-DD HH24:MI:SS.FF'));
+    DBMS_OUTPUT.PUT_LINE('TIMESTAMP WITH TIME ZONE: ' || TO_CHAR(my_tz, 'YYYY-MM-DD HH24:MI:SS.FF TZR'));
+    DBMS_OUTPUT.PUT_LINE('TIMESTAMP WITH LOCAL TIME ZONE: ' || TO_CHAR(my_tsl, 'YYYY-MM-DD HH24:MI:SS.FF'));
+END;
+/
+
+
