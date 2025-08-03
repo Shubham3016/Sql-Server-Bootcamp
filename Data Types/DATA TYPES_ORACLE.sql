85a064edf542ebe1f61409610d9d9c88bf250cf7
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2025-08-03 20:25:19 +0530

    Update DATA TYPES_ORACLE.sql

--- Data Types/DATA TYPES_ORACLE.sql
+++ Data Types/DATA TYPES_ORACLE.sql
@@ -1,3 +1,13 @@
+
+------------------------------------------------------------------------
+--Exact numeric data types
+------------------------------------------------------------------------
+
+--This data type will store exact numbers such as Number Integer. 
+--They have their own lower, upper limit, and memory requirements.
+--They have their own memory needs, as well as a lower and upper limit. 
+--It is also necessary to use the lowest data type to save memory requirements. 
+
 -- NUMBER Example (Generic numeric type)
 DECLARE
     n NUMBER(5) := 12345;
